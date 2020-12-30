84e3b8767216e0aef26626f63bdb2cc4817fa8e2
KebS Pericles <kebsebastian@gmail.com>
2020-12-29 21:02:12 -0600

    Chat bonito

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var clientes = [];
+var mensajes = [];
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+server.listen(8080, () => console.log('Servidor iniciado en 8080'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+app.get('/chat/:username', function(req, res) {
+    res.sendFile(__dirname + '/public/chat.html');
+});
+
+app.post('/login', function(req, res) {
+    let username = req.body.username;
+    let id = req.body.id;
+    clientes.push({ id: id, username: username });
+    let iniciar = { clientes: clientes, mensajes: mensajes };
+    io.emit('socket_conectado', { id: id, username: username });
+    return res.json(iniciar);
+});
+
+app.post('/send', function(req, res) {
+    let username = req.body.username;
+    let id = req.body.id;
+    let msg = req.body.text;
+    mensajes.push({ id: id, msg: msg, username: username });
+    io.emit('mensaje', { id: id, msg: msg, username: username });
+    return res.json({ text: 'Mensaje enviado.' });
+});
+
+io.on('connection', socket => {
+    console.log('Socket conectado', socket.id);
+    socket.on('disconnect', () => {
+        clientes = clientes.filter(cliente => cliente.id != socket.id);
+        if (clientes.length == 0) {
+            mensajes = [];
+        }
+        io.emit('socket_desconectado', { texto: 'Socket desconectado.', id: socket.id });
+    });
+});
